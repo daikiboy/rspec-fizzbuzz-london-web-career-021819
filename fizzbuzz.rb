755a24d68a1dc7c80973d3b92f99e3f2b3cf8e66
simone.rainieri <simone.rainieri@gmail.com>
2019-02-06 20:03:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 != 0 # if the number int is divisible by 3
     return "Fizz"
-    elsif int % 5 == 0
+    elsif int % 5 == 0 && int % 3 != 0
     return "Buzz"
     elsif int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
